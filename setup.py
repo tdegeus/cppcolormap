9c4ae8cf8a33aade1e9173a0b2cbcb08e906a8c7
Tom de Geus <tom@geus.me>
2023-10-12 09:58:26 +0200

    Using `pyproject.toml`

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
-from pathlib import Path
-
 from setuptools_scm import get_version
 from skbuild import setup
 
 project_name = "cppcolormap"
 
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
-
 setup(
     name=project_name,
     description="Library with colormaps",
-    long_description="Library with colormaps",
-    keywords="colormap, plot, matplotlib",
     version=get_version(),
     license="MIT",
     author="Tom de Geus",
@@ -22,4 +15,5 @@ setup(
     package_dir={"": "python"},
     cmake_install_dir=f"python/{project_name}",
     cmake_minimum_required_version="3.13",
+    python_requires=">=3.6",
 )
